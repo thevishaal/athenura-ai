443222a6de12daa14ef165f665d55773b7c65464
Jeetendra Dhangar <jeetendradhangar6@gmail.com>
2026-04-05 16:59:44 +0530

    bug fixed

--- athenura_ai/settings.py
+++ athenura_ai/settings.py
@@ -150,7 +150,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
